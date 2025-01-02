1cca7aadcdc34100e6a2b343c289374b94c402c5
Yeisson Venencia <yvenencia@assanet.com>
2025-01-01 22:36:30 -0400

    [DAY-01] initial commit AoC 2015

---
+++ advent-of-code-2015/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using advent_of_code_2015.challenges;
+
+Day01Challenges.SolvePart02();
---
+++ advent-of-code-2015/challenges/Day01Challenges.cs
@@ -0,0 +1,40 @@
+namespace advent_of_code_2015.challenges;
+
+public class Day01Challenges
+{
+    private static string ReadInputData()
+    {
+        var data = File.ReadAllText("../../../inputs/input_01.txt");
+        
+        return data;
+    }
+    public static void SolvePart01()
+    {
+        var data = ReadInputData();
+        var totalUp = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            totalUp += (data[i] == '(') ? 1 : 0;
+        }
+
+        var totalDown = data.Length - totalUp;
+        Console.WriteLine("Result part 01: {0}", totalUp - totalDown);
+    }
+    
+    public static void SolvePart02()
+    {
+        var data = ReadInputData();
+        var currentFloor = 0;
+        var instruction = -1;
+        for (int i = 0; i < data.Length; i++)
+        {
+            currentFloor += (data[i] == '(') ? 1 : -1;
+            if (currentFloor == -1)
+            {
+                instruction = i + 1;
+                break;
+            }
+        }
+        Console.WriteLine("Result part 02: {0}", instruction);
+    }
+}
